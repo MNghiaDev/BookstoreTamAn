eb9fd8bc50aceb7ece458d73f4df32fde579a054
MNghiaDev <minhnghia11202@gmail.com>
2025-03-18 10:33:43 +0700

    Initialize Angular & Spring Boot project

---
+++ backend/BookstoreTamAn/src/main/java/com/minhnghia/datn/BookstoreTamAn/BookstoreTamAnApplication.java
@@ -0,0 +1,13 @@
+package com.minhnghia.datn.BookstoreTamAn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreTamAnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreTamAnApplication.class, args);
+	}
+
+}
---
+++ backend/BookstoreTamAn/src/test/java/com/minhnghia/datn/BookstoreTamAn/BookstoreTamAnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhnghia.datn.BookstoreTamAn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreTamAnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
