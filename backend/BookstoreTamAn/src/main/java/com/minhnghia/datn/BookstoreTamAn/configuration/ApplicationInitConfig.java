5f2b91243f01b07a874071cbfb9c765d92d33444
MNghiaDev <minhnghia11202@gmail.com>
2025-04-16 20:00:03 +0700

    Init Application

--- backend/BookstoreTamAn/src/main/java/com/minhnghia/datn/BookstoreTamAn/configuration/ApplicationInitConfig.java
+++ backend/BookstoreTamAn/src/main/java/com/minhnghia/datn/BookstoreTamAn/configuration/ApplicationInitConfig.java
@@ -1,4 +1,38 @@
 package com.minhnghia.datn.BookstoreTamAn.configuration;
 
+import com.minhnghia.datn.BookstoreTamAn.model.User;
+import com.minhnghia.datn.BookstoreTamAn.repository.UserRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE,makeFinal = true)
 public class ApplicationInitConfig {
+    private static final Logger log = LoggerFactory.getLogger(ApplicationInitConfig.class);
+    PasswordEncoder passwordEncoder;
+
+    @Bean
+    ApplicationRunner applicationRunner(UserRepository userRepository){
+        return args -> {
+            if(!userRepository.existsByUsername("admin")){
+                String role = "admin";
+                User user = User.builder()
+                        .username("admin")
+                        .password(passwordEncoder.encode("admin"))
+                        .role(role)
+                        .build();
+                userRepository.save(user);
+                log.warn("admin user has been created with default password: admin, please change password");
+            }
+        };
+    }
+
 }
